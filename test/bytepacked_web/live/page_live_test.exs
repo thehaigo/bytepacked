b9e1754d577f839c142d91110aa1197b89e80a32
thehaigo <sirannha@gmail.com>
2021-08-05 00:01:56 +0900

    top page create

--- test/bytepacked_web/live/page_live_test.exs
+++ test/bytepacked_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule BytepackedWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "Welcome to Bytepacked"
+    assert render(page_live) =~ "Welcome to Bytepacked"
   end
 end
